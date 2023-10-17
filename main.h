3dc9a4fd7b5130b613d07c3654468971b9edc7cb
Dura07 <oyedejioluwasheye2013@gmail.com>
2023-10-17 08:51:30 -0700

    print

--- main.h
+++ main.h
@@ -6,20 +6,7 @@
 #include <unistd.h>
 #include <limits.h>
 
-typedef struct format
-{
-	char *id;
-	int (*f)();
-} match;
-
-int _putchar(char c);
 int _printf(const char *format, ...);
-int printf_char(va_list val);
-int printf_string(va_list val);
-int _strlen(char *str);
-int _strlenc(const char *str);
-int print_37(void);
-int print_revs(va_list val);
-int print_rot13(va_list val);
+int _putchar(char c);
 
 #endif
---
+++ printf.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * _printf - Produces output according to a format.
+ * @format: The format string.
+ *
+ * Return: The number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	int numeric_count = 0;
+	va_list argument;
+	char *str_arg;
+	char char_arg;
+
+	va_start(argument, format);
+
+	for (numeric_count = 0; format && format[numeric_count]; numeric_count++)
+	{
+	if (format[numeric_count] == '%')
+	{
+		numeric_count++;
+		if (format[numeric_count] == 'c')
+		{
+			char_arg = va_arg(argument, int);
+			_putchar(char_arg);
+		}
+		else if (format[numeric_count] == 's')
+		{
+			str_arg = va_arg(argument, char *);
+	while (*str_arg)
+			{
+	_putchar(*str_arg);
+	str_arg++;
+			}
+		}
+		else if (format[numeric_count] == '%')
+		{
+			_putchar('%');
+		}
+	}
+	else
+	{
+		_putchar(format[numeric_count]);
+	}
+
+}
+
+va_end(argument);
+return (numeric_count);
+}
