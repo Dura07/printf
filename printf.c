c437ecfd66db6d3baf145426adecb7c05d5378cc
Dura07 <oyedejioluwasheye2013@gmail.com>
2023-10-17 09:31:56 -0700

    print

--- printf.c
+++ printf.c
@@ -8,44 +8,44 @@
  */
 int _printf(const char *format, ...)
 {
-	int numeric_count = 0;
-	va_list argument;
+	int count = 0;
+	va_list args;
 	char *str_arg;
 	char char_arg;
 
-	va_start(argument, format);
+	va_start(args, format);
 
-	while (format && format[numeric_count])
+	while (format && format[count])
 	{
-	if (format[numeric_count] == '%')
+	if (format[count] == '%')
 	{
-	numeric_count++;
-	if (format[numeric_count] == 'c')
+	count++;
+	if (format[count] == 'c')
 	{
-		char_arg = va_arg(argument, int);
-		_putchar(char_arg);
+	char_arg = va_arg(args, int);
+	_putchar(char_arg);
 	}
-	else if (format[numeric_count] == 's')
+	else if (format[count] == 's')
 	{
-	str_arg = va_arg(argument, char *);
+	str_arg = va_arg(args, char *);
 	while (*str_arg)
 	{
 	_putchar(*str_arg);
 	str_arg++;
 	}
 	}
-	else if (format[numeric_count] == '%')
+	else if (format[count] == '%')
 	{
 	_putchar('%');
 	}
 	}
 	else
 	{
-		_putchar(format[numeric_count]);
+		_putchar(format[count]);
 	}
-	numeric_count++;
+	count++;
 	}
 
-	va_end(argument);
-	return (numeric_count);
+	va_end(args);
+	return (count);
 }
