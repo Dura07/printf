4fed6e6742c6df9c8e38a034fb05bc5e311c3836
Dura07 <oyedejioluwasheye2013@gmail.com>
2023-10-17 10:42:31 -0700

    Print f

--- printf.c
+++ printf.c
@@ -11,8 +11,10 @@ int _printf(const char *format, ...)
 {
 	int numeric_count = 0;
 	va_list argument;
+	int printed_chars = 0;
 
 	va_start(argument, format);
+
 
 	while (format && format[numeric_count])
 	{
@@ -23,7 +25,7 @@ int _printf(const char *format, ...)
 			{
 				char char_arg = va_arg(argument, int);
 
-				write(1, &char_arg, 1);
+				printed_chars += write(1, &char_arg, 1);
 			}
 			else if (format[numeric_count] == 's')
 			{
@@ -31,22 +33,18 @@ int _printf(const char *format, ...)
 
 				while (*str_arg)
 				{
-					write(1, str_arg, 1);
+					printed_chars += write(1, str_arg, 1);
 					str_arg++;
 				}
 			}
 			else if (format[numeric_count] == '%')
-			{
-				write(1, "%", 1);
-			}
+				printed_chars += write(1, "%", 1);
 		}
 		else
-		{
-			write(1, &format[numeric_count], 1);
-		}
+			printed_chars += write(1, &format[numeric_count], 1);
 		numeric_count++;
 	}
 
 	va_end(argument);
-	return (numeric_count);
+	return (printed_chars);
 }
